90e38fc6efc065645254743169ac4fa6981f310b
Shiruvati <mrnattyboy05@gmail.com>
2025-01-15 18:22:46 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,4 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.0.2]">
-      <HeroSection />
-   
+     
